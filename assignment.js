cea54960a056f4b26f5712ddbb417d42c34aa3bd
Nasir Uddin <monforingnasir@gmail.com>
2021-01-22 08:34:02 +0600

    add kilometer to meter

---
+++ assignment.js
@@ -0,0 +1,8 @@
+// kilometer to Meter Function
+function kilometerToMeter(kiloMeter) {
+  var kilo = 1000;
+  var meter = kilo * kiloMeter;
+  return meter;
+}
+var output = kilometerToMeter(2);
+console.log(output);
